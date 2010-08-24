4e998251ab32ef3db26e627fc7435c19c45daf4e
frank.wunderlich <frank.wunderlich@254c0351-e948-6d7a-0b78-ee9e900393d8>
2010-08-24 12:01:41 +0000

    commit-script posts username

--- trunk/svn_commit.sh
+++ trunk/svn_commit.sh
@@ -4,5 +4,5 @@ read Message
 #add all new files
 svn status | grep "^?" | cut -d ? -f 2 | xargs svn add
 
-svn ci ./ --message "$Message"
+svn ci ./ --message "$Message" --username frank.wunderlich
 read _wait_
