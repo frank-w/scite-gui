8918db9444d2452ad75b97af54c8225d1dd25294
frank.wunderlich <frank.wunderlich@254c0351-e948-6d7a-0b78-ee9e900393d8>
2010-08-24 12:11:37 +0000

    changed start_scite.sh to load gui.lua as startup-script

--- start_scite.sh
+++ start_scite.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+GUI_DIR=$(pwd)
 cd Scite_patch/scite_src/src/scite/bin/
 
-./SciTE -check.if.already.open=0 -save.session=0
+./SciTE -check.if.already.open=0 -save.session=0 -ext.lua.startup.script=$GUI_DIR/gui.lua
 
 echo press any key to close window...
 read _wait_
