93bcf56b1138836016e8e1c0e9d66ae38c6ec3a9
frank.wunderlich <frank.wunderlich@254c0351-e948-6d7a-0b78-ee9e900393d8>
2010-09-30 18:58:34 +0000

    fix bug in export-script

--- svn_export.sh
+++ svn_export.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-version=$(svnversion)
+version=$(svnversion -n|sed -e 's/.*:\([0-9]*\).*/\1/')
+echo "Revision: $version"
 svn export ./ ../_sg_
 cd ../_sg_
 7za u -tzip $(dirname $0)/scite-gui_r$version.zip *
