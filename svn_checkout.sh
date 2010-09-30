86c2a69cebc1648577fd6041821296284c90d0ce
frank.wunderlich <frank.wunderlich@254c0351-e948-6d7a-0b78-ee9e900393d8>
2010-09-30 18:10:14 +0000

    moved svn-scripts to root, added export/pack-script

--- svn_checkout.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-svn co https://scite-gui.googlecode.com/svn/trunk/ ./
-read _wait_
--- svn_ignore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-svn -R propset svn:ignore -F svn_ignore .
-
-echo press any key to close window...
-read _wait_
